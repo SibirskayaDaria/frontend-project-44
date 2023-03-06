ed5fa1884b7f91a26fc9a3e31714ea77c0821d16
SibirskayaDaria <sdaryserg@mail.ru>
2023-03-06 15:49:28 +0200

    add brain-prime.js

---
+++ src/brain-prime.js
@@ -0,0 +1,26 @@
+import playGame from '../src/index.js';
+import generateRandomInt  from '../src/utils.js';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const isPrimeNumber = (number) => {
+  for (let a = 2; a < number; a += 1) {
+    if (number % a === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const randomInt = generateRandomInt (2, 50);
+  const correctAnswer = isPrimeNumber(randomInt) ? 'yes' : 'no';
+  const question= `Question: ${randomInt}\nYour answer: `;
+
+  return [question, correctAnswer];
+};
+
+const start = () => {
+  playGame(getQuestionAndAnswer, gameDescription);
+};
+export default start;
