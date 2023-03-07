b1bd062313c38ac26e6c1633fc99afb66b045030
SibirskayaDaria <sdaryserg@mail.ru>
2023-03-07 16:41:36 +0200

    add npx eslint 2

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,6 @@
 export default function generateRandomInt(min, max) {
   const smallestValue = Math.ceil(min);
   const highestValue = Math.floor(max);
-return Math.floor(Math.random() * (highestValue - smallestValue)) + smallestValue;
-};
-  
+ return Math.floor(Math.random() * (highestValue - smallestValue)) + smallestValue;
+ };
+ 
