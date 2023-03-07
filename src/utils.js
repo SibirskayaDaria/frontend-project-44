f429d6b64f6cd392e0ddb0747349aa96c40a5b00
SibirskayaDaria <sdaryserg@mail.ru>
2023-03-07 16:46:15 +0200

    add npm eslint

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,5 @@
 export default function generateRandomInt(min, max) {
   const smallestValue = Math.ceil(min);
   const highestValue = Math.floor(max);
- return Math.floor(Math.random() * (highestValue - smallestValue)) + smallestValue;
- };
- 
+  return Math.floor(Math.random() * (highestValue - smallestValue)) + smallestValue;
+}
