1944282bae2f3f9bb1f75a9d5d4cae9f1f08beb1
SibirskayaDaria <sdaryserg@mail.ru>
2023-03-07 15:09:45 +0200

    brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,32 +4,37 @@ import generateRandomInt from '../utils.js';
 import playGame from '../index.js';
 
 const gameDescription = 'What number is missing in the progression?';
-const progressionLength = 10;
 
-const aritmeticProgression = () => {
-    let progressionNumber = generateRandomInt(0, 50);
-    const step = generateRandomInt (2,5);
-    const progression = [];
-
-for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(progressionNumber);
-    progressionNumber += step;
+const getNumberSeries = (start, step, length, store = []) => {
+  let i = 0;
+  let nextNumber = start;
+  while (i < length) {
+    nextNumber += step;
+    store.push(nextNumber);
+    i += 1;
   }
-  return progression;
 };
 
-const getQuestionAndAnswer = () => {
-  const progression = aritmeticProgression();
-  const randomIndex = generateRandomInt(0, progressionLength);
+const startBrainProgressionCore = () => {
+  const numbersSeries = [];
+  const randomInt = generateRandomInt(1, 100);
+  const progressionStep = generateRandomInt(1, 11);
+  const progressionLength = generateRandomInt(5, 11);
 
-  const correctAnswer = String(progression[randomIndex]);
-  progression[randomIndex] = '..';
-  const question = `Question: ${progression}\nYour answer: `;
+  getNumberSeries(randomInt, progressionStep, progressionLength, numbersSeries);
+
+  const replacementNumber = generateRandomInt(0, numbersSeries.length);
+  const hiddenNumber = numbersSeries[replacementNumber];
+  const correctAnswer = String(hiddenNumber);
+  numbersSeries[replacementNumber] = '..';
+  const questionOutput = numbersSeries.join(' ');
+  const question = `Question: ${questionOutput}\nYour answer: `;
 
   return [question, correctAnswer];
 };
 
 const start = () => {
-  playGame(getQuestionAndAnswer, gameDescription);
+  playGame(startBrainProgressionCore, gameDescription);
 };
+
 export default start;
