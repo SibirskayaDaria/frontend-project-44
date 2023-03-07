9289d1b89af8d060c6b4a75b968dcd82520a43ba
SibirskayaDaria <sdaryserg@mail.ru>
2023-03-07 14:43:22 +0200

    add way out brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import generateRandomInt from '../utils.js';
-import playGame from '../index.js';
+import generateRandomInt from '../src/utils.js';
+import playGame from '../src/index.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
