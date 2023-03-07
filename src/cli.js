1e2ad9a21cf1fab3498b9fa14b4b33972e6aae07
SibirskayaDaria <sdaryserg@mail.ru>
2023-03-07 09:21:33 +0200

    add cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
- 
-const usernameQuestion = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello ${userName}!`);
-    return userName;
-  };
-  
-  export default usernameQuestion;
+
+const greet = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  return userName;
+};
+
+export default (greet);
